0eb56f5c8b4bca0de40d3e9efb3d93b3b6e6a6c5
saeed90webdev <saeed90.webdeveloper@gmail.com>
2025-05-07 16:20:40 +0330

    add templates and static and media confugs

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOTS = BASE_DIR/'static'    
+STATICFILES_DIRS = [BASE_DIR/'static',]
+
+
+# Media Files
+MEDIA_URL = '/media/'
+MEDIA_ROOTS = BASE_DIR/'media' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
